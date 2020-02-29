a0df9eae771f0181692a69237a9539e24f730ca5
Michael M <mickelsonmichael@gmail.com>
2020-02-29 11:29:11 -0700

    Add Service Tests project

---
+++ Bayshore/Bayshore.Service.Test/PallindromeServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bayshore.Service.Test
+{
+    public class PallindromeServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
